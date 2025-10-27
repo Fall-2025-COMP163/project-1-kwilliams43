934faf9a6470cb595132b0e124cd238564a21e6c
kwilliams43 <kwilliams43@aggies.ncat.edu>
2025-10-27 13:24:59 -0400

    Update project1_starter.py

--- project1_starter.py
+++ project1_starter.py
@@ -90,6 +90,15 @@ def load_character(filename):
     with open(filename,'r') as file:
         lines = f.readlines()
 
+    data = {}
+    for line in lines:
+        key, value= line.strip().split(': ', 1)
+        data[key] = value
+
+    character = {'name': data['Character Name'], 'class': data['Class']}, 'level': int(data['Level']), 'strength': int(data['Strengh']), 'Magic': int(data['Magic'], 'health': int(data['Health']), 'gold': int(data['gold'])}
+    return character
+                                    
+
 
     
         
@@ -111,8 +120,17 @@ def display_character(character):
     Health: 80
     Gold: 100
     """
-    # TODO: Implement this function
-    pass
+    print('=== CHARACTER SHEET ===')
+    print(f'Name: {character['name']}')
+    print(f'Class: {character['class']}')
+    print(f'Level: {character['level']}')
+    print(f'Strength: {character['strength']}')
+    print(f'Magic: {character['magic']}')
+    print(f'Health: {character['health']}')
+    print(f'Gold: {character['gold']}')
+    print('========================')
+
+    
 
 def level_up(character):
     """
@@ -120,17 +138,27 @@ def level_up(character):
     Modifies the character dictionary directly
     Returns: None
     """
-    # TODO: Implement this function
-    # Remember to recalculate stats for the new level
-    pass
+    character['level'] += 1
+    strength, magic, health = calculate_stats(character['class'], character['level'])
+    character['strength'] = strength
+    character['magic'] = magic
+    character['health'] = health
+    print(f'{character['name']} leveled up to level {character['level']}!')
+    
 
 # Main program area (optional - for testing your functions)
 if __name__ == "__main__":
     print("=== CHARACTER CREATOR ===")
-    print("Test your functions here!")
-    
-    # Example usage:
-    # char = create_character("TestHero", "Warrior")
-    # display_character(char)
-    # save_character(char, "my_character.txt")
-    # loaded = load_character("my_character.txt")
+    name = input("Enter Your Character's name: ")
+    choose_class = input(" Choose a CLass (Warrior, Mage, Rouge, Cleric): ")
+    char = create_character(name, choose_class)
+    display_character(char)
+    filename= f'{char['name']}_save.txt'
+    save_character(char, filename)
+    print(f'Character saved to {filename}')
+    print('Loading character...')
+    loaded_char = (char,  filename)
+    display_character(loaded_char)
+    print('Leveling Up your character...')
+    level_up(loaded_char)
+    display__character(loaded_char)
